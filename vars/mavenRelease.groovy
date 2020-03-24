d2cc13bcb414698a4f89181bb6076eb6ca841f20
anandjainsa <53891867+anandjainsa@users.noreply.github.com>
2020-03-24 19:22:56 +0530

    Update mavenRelease.groovy

--- vars/mavenRelease.groovy
+++ vars/mavenRelease.groovy
@@ -3,6 +3,6 @@ def call(RELVER)
  script {
     def pom = readMavenPom file: 'pom.xml'
     def version = pom.version.replace("-SNAPSHOT", ".${RELVER}")
-    sh "/var/lib/jenkins/tools/hudson.tasks.Maven_MavenInstallation/Maven_3.3.9/bin/mvn -DreleaseVersion=${version} -DpushChanges=false -DlocalCheckout=true -DpreparationGoals=initialize release:prepare release:perform -B"
+    sh "/var/lib/jenkins/tools/hudson.tasks.Maven_MavenInstallation/Maven_3.3.9/bin/mvn -DreleaseVersion=${version} -DdevelopmentVersion=${pom.version} -DpushChanges=false -DlocalCheckout=true -DpreparationGoals=initialize release:prepare release:perform -B"
         } 
 }
